a52c0f1822cc4dc955cd5d422b18803dc4b0095b
Joshua Morton <joahua.morton13@gmail.com>
2014-04-27 05:27:57 -0400

    updated databases with the database viewer class that cannot edit the db

--- databases.py
+++ databases.py
@@ -53,7 +53,7 @@ class Database:
         adds a user to the database, by initializing them with an array of 0s
         """
 
-        #check to see a user of the same name is in the database first
+        #TODO: check to see a user of the same name is in the database first
         values = [0 for x in xrange(self.size)]
         self.cursor.execute("INSERT INTO {table} VALUES ('{user}', '{values}')".format(table = self.table, user = user, values = values, username = self.username))
         self.db.commit()
@@ -81,11 +81,14 @@ class Database:
         return values
 
     def setUser(self, user, values):
+        """
+        """
         self.cursor.execute("UPDATE {table} SET {thing}='{values}' WHERE {username} = '{user}'".format(table = self.table, user = user, username = self.username, thing = self.field, values = values))
         self.db.commit()
 
 
-        
+        """
+        """
     def updateItem(self, user, item, value):
         values = getUser(user)
         values[item] = value
@@ -105,6 +108,41 @@ class Database:
         if len(vals) < self.size:
             vals += [0 for x in xrange(self.size-len(vals))]
         return vals
+
+
+class Viewer:
+    """
+    Basically a Database object, except it can't edit existing databases (without someone doing...things)
+    """
+
+    def __init__(self, name, table, size):
+
+        """
+        var name - the filename of the database to be connected to
+        var table - 
+        var size - the number of things that users can like or dislike
+        """
+
+        self.field = "array"
+        self.username = "username"
+        self.databaseFolder = "data"
+        self.name = name
+        self.table = table
+        self.db = sqlite3.connect(os.path.join(self.databaseFolder,self.name))
+        self.cursor = self.db.cursor()
+        self.size = size
+
+    def getUser(self, user):
+
+        """
+        returns the array of values (converted to python-readable data) for a given user
+        """
+
+        self.cursor.execute("SELECT * FROM {table} WHERE {username} = '{user}'".format(table = self.table, user = user, username = self.username))
+        values = self.cursor.fetchone()[1]
+        values = self._padValues(values)
+        return values
+
 
 if __name__ == "__main__":
     thing = Database("persistent.db", "opinions", 9)
