dcff6ca317757740bfecf66f1da7941b2ab53342
Joshua Morton <joshua.morton13@gmail.com>
2014-05-21 14:09:54 +0200

    set up the skeleton for the new CollabFilter code

--- CollabFilter.py
+++ CollabFilter.py
@@ -13,8 +13,51 @@ class CollaborativeFilter:
     A Collaborative Filtering algorithm/object meant to be heavily optimized for speed
     """
 
-    def __init__(self, *args):
+    def __init__(self, name, path, table, debug=False, cache=True):
         """
+        Initilialies the collaborative filter with the values
+
+        Arguments:
+            name  -> the name of the database file
+            path  -> the path to the database file
+            table -> the name of the table in the database file
+            debug -> activates debug mode, providing additional outputs and information about what is happening
+            cache -> used for testing, to compare between the caches being used and a normal, cache free version
+                        both for benchmarking and for error checking
         """
 
-        self.
+
+        self.name = name
+        self.path = path
+        self.table = table
+        self.debug = debug
+        self.cache = cache
+        self.db = databases.Database(name, table, path)
+
+        #implemented as map(user->map(item->opinion))
+        #opinions can be None or 1-5
+        self.opinions = {}
+
+        #implemented as map(user1->map(user2->tuple(rss(u1), rss(u2), multSum(u1, u2))))
+        #rss(u) is root sum squared
+        #multSum(u1, u2) is sum(u1[item]*u2[item] for item in sharedItems)
+        self.similarities = {}
+
+        #implemented as map(user1->map(user2->tuple(sum(simil*r for user in users), sum(simil for user in users))))
+        self.calculated = {}
+
+    def fetchOpinion(self, user, item):
+        """
+        """
+        pass
+
+
+    def predictOpinion(self, user, item):
+        """
+        """
+        pass
+
+    def changeOpinion(self, user, item, opinion):
+        """
+        """
+        pass
