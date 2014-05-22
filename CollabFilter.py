f950f744b1ea8959ae4a32a424a57c24d6c9da64
Joshua Morton <joshua.morton13@gmail.com>
2014-05-22 19:19:32 +0200

    Updated the CollabFilter program, still in progress but most of the getOpinion is now working

--- CollabFilter.py
+++ CollabFilter.py
@@ -3,7 +3,6 @@ The collaborative filtering portion of the system.
 Should always be used with databases.database
 """
 
-import math
 
 import databases.database as databases
 import math
@@ -32,7 +31,7 @@ class CollaborativeFilter:
         self.table = table
         self.debug = debug
         self.cache = cache
-        self.db = databases.Database(name, table, path)
+        self.db = databases.Database(self.name, self.table, self.path)
         self.users = {} #the set of users in the database
 
         #implemented as map(user->map(item->opinion))
@@ -69,14 +68,18 @@ class CollaborativeFilter:
             user -> the user whose opinion is to be calculated
             item -> the item of which the user has an unknown opinion
         """
-        if (self.debug == False):
+        if (self.cache == True):
             if user in self.calculated:
                 if item in self.calculated[user]:
                     return _ratingFromCalculated(*self.calculated[user][item])
-                else:
-                    self.calculated[user][item] = (self._ratingTop(user, item), self._ratingBottom(user, item))
+                else: #if item not in self.calculated[user]
+                    self.calculated[user][item] = self._calculateWeights(user, item)
                     return _ratingFromCalculated(*self.calculated[user][item])
-                #more, so much more!
+            else: #if user note in self.calculated
+                self.calculated[user] = {}
+                self.calculated[user][item] = self._calculateWeights(user, item)
+        else: #if calculation is forced
+            pass #for now
 
 
     def _ratingFromCalculated(weightedRatings, sumSimilarities):
@@ -97,28 +100,97 @@ class CollaborativeFilter:
         return float(weightedRatings) / sumSimilarities
 
 
-    def _ratingTop(self, user, item):
+    def _calculateWeights(self, user, item):
         """
-        returns the top portion of the calculated opinion
+        returns a tuple (self._ratingTop(user, item), self._ratingBottom(user, item))
+        """
+        
+        #create the users and items sets
+        users = {person[0] for person in self.dbViewer.users()} - {user}
+
+        #use those sets to seperately calculate the top and bottom values for the final rating
+        return (self._ratingTop(user, item, users), self._ratingBottom(user, item, users))
+
+
+    def _ratingTop(self, user, item, users):
+        """
+        Returns the top portion of the calculated opinion
 
         Arguments:
             user -> the user whose opinion is to be calculated
             item -> the item of which the user has an unknown opinion
         """
-        return sum(self._similarity(user, other) * self._opinion(other, item) for other in {users - user})
+
+        #get the items shared by both users
+        return sum(self._similarity(user, other) * self._opinion(other, item) for other in users)
 
 
-    def _ratingBottom(self, user, item):
+    def _ratingBottom(self, user, item, users):
         """
 
         Arguments:
             user -> the user whose opinion is to be calculated
             item -> the item of which the user has an unknown opinion
         """
-        return sum(self._similarity(user, other) for other in {users - user})
+        return sum(self._similarity(user, other) for other in users)
+
+
+    def _similarity(self, user, other):
+        """
+        Arguments:
+            user  -> the user whose opinion is being calculated
+            other -> the other user to whom the user is being compared
+
+        """
+        
+        if (self.cache == True):
+            if user in self.similarities:
+                if other in self.similarities[user]:
+                    return self._calculateSimilarity(*self.similarities[user][other])
+                else:
+                    self.similarities[user][other] = self._setSimilarities(user, other)
+                    return self._calculateSimilarity(*self.similarities[user][other])
+            else:
+                self.similarities[user] = {}
+                self.similarities[user][other] = self._setSimilarities(user, other)
+                return self._calculateSimilarity(*self.similarities[user][other])
+        else:
+            pass #for now
+
+
+    def _calculateSimilarity(self, rssUser, rssOther, multSum):
+        """
+        calculates the similarity between users based on the rss and multSum values
+        """
+
+        return multSum / (rssUser * rssOther)
         
 
+    def _setSimilarities(user, other):
+        """
+        """
+        pass
 
+
+    def _opinion(self, user, item):
+        """
+        gets the opinion a user has about an item from the database
+        
+        Arguments:
+            user -> a user whose opinion we want
+            item -> the item for which we want their opinion
+    
+        """
+        if user in self.opinions:
+            if item in self.opinions[user]:
+                return self.opinons[user][item]
+            else:
+                self.opinions[user][item] = db.currentOpinion(user, item) or 0
+                return self.opinions[user][item]
+        else:
+            self.opinions[user] = {}
+            self.opinons[user][item] = db.currentOpinion(user, item) or 0
+            return self.opinions[user][item]
 
 
     def changeOpinion(self, user, item, opinion):
@@ -129,5 +201,4 @@ class CollaborativeFilter:
 
     
 if __name__=="__main__":
-    cf = CollaborativeFilter
-    print cf
+    cf = CollaborativeFilter("database.db", "databases/data", "main")
