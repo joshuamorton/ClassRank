785e4641c818e2c4bc832e85815a1d0a2a211117
Joshua Morton <joshua.morton13@gmail.com>
2014-05-31 21:57:11 +0200

    added a basic tornado webapp code to deal with and fool around with later

---
+++ webBase.py
@@ -0,0 +1,18 @@
+"""
+a
+"""
+
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("hello world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
