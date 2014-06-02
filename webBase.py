ab803e495d2c21c9f913f5eb46329e02efd2be3f
Joshua Morton <joshua.morton13@gmail.com>
2014-06-02 02:35:35 +0200

    updated webBase.py, slowly learning tornado's syntax and now its starting to makes sense

--- webBase.py
+++ webBase.py
@@ -9,9 +9,18 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("hello world")
 
-application = tornado.web.Application([
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, id):
+        self.write(id)
+
+
+
+applicationee = tornado.web.Application([
+    (r"/index/(\d+)/", IndexHandler),
     (r"/", MainHandler),
 ])
+
+
 
 if __name__ == "__main__":
     application.listen(8888)
