979d41b6b3d0a137a41e148624e9c9bf8aeab822
Joshua Morton <joshua.morton13@gmail.com>
2014-07-03 14:09:42 +0200

    Implemented the 8 fetch methods

--- databases/database-new.py
+++ databases/database-new.py
@@ -33,19 +33,16 @@ class Database(object):
 
     def __init__(self, name="ClassRank.db", table="main", folder="data", uid="user_id", hashlength=64):
         """
-        Initializes the database, creates it as necessary
+        initializes the database, creates tables and files as necessary
 
         Keyword Arguments:
             name -- the name of the database file, created as necessary
-                default: "ClassRank.db"
             table -- the table, a holdover from previous versions, can be ignored
-                default: "main"
             folder -- a relative path to the folder where the database should be stored 
-                default: "data"
             uid -- the value under which the database stores the user's Primary Key, holdover can be safely ignored
-                default: user_id
             hashlength -- the length of password hashes
-                default: 64
+
+        The system defaults to name="ClassRank.db", table="main" folder="data", uid="user_id, and hashlength="64"
         """
         self.engine = sqlalchemy.create_engine('sqlite:///'+folder+'/'+name)
 
@@ -70,7 +67,7 @@ class Database(object):
         database session factory wrapper
 
         Provide a transactional scope around a series of operations.  use the syntax
-            `with self.session_scope() as session: #or similar`
+            `with Database.session_scope() as session: #or similar`
         for operations requiring a session
         """
         session = self.sessionmaker()
@@ -87,46 +84,246 @@ class Database(object):
     #methods that do things!
     def fetch_school_by_name(self, school):
         """
+        returns a school object for a given name
+
+        Searches the database for a school object with the given short name ("Georgia Tech")
+        This search is "unsafe" and will therefore throw errors if the school does not exist
+
+        Arguments:
+            school -- the short name of the school to be found
+
+        Errors:
+            ItemDoesNotExistError -- rasied if there is no school found
+            MultipleResultsFound -- rasied if there are multiple items of the same name (which should be unique) in the database, indicative of major issues
+
+        Return:
+            A school object as defined in the SchoolDatabase class
         """
-        pass
+
+        with self.session_scope() as session:
+            try:
+                return session.query(self.school).filter(self.school.school_short == school_name).one()
+            except sqlalchemy.orm.exc.NoResultFound:
+                raise ItemDoesNotExistError(DatabaseObjects.School, school)
+        
 
     def fetch_school_by_id(self, schoolid):
         """
+        returns a school object by a given schoolid
+
+        Searches the database for a school object with the given unique id (12345)
+        This search is "unsafe" and will therefore throw errors if the school does not exist
+
+        Arguments:
+            schoolid -- the unique id of the school in the database
+
+        Errors:
+            ItemDoesNotExistError -- rasied if there is no school found
+            MultipleResultsFound -- rasied if there are multiple items of the same id (which should be unique) in the database, indicative of major issues
+
+        Return:
+            A school object as defined in the SchoolDatabase class
         """
-        pass
+
+        with self.session_scope() as session:
+            try:
+                return session.query(self.school).get(schoolid)
+            except sqlalchemy.orm.exc.ObjectDeletedError:
+                raise ItemDoesNotExistError(DatabaseObjects.School, schoolid)
+
 
     def fetch_user_by_name(self, username):
         """
+        returns a user object for a given username
+
+        Searches the database for a user object with the given username ("jmorton")
+        This search is "unsafe" and will therefore throw errors if the user does not exist
+
+        Arguments:
+            username -- the short name of the school to be found
+
+        Errors:
+            ItemDoesNotExistError -- rasied if there is no item found
+            MultipleResultsFound -- rasied if there are multiple items of the same name (which should be unique) in the database, indicative of major issues
+
+        Return:
+            A user object as defined in the UserDatabase class
         """
-        pass
+
+        with self.session_scope() as session:
+            try:
+                return session.query(self.user).filter(self.user.user_name == username).one()
+            except sqlalchemy.orm.exc.NoResultFound:
+                raise ItemDoesNotExistError(DatabaseObjects.User, username)
+
 
     def fetch_user_by_id(self, userid):
         """
+        returns a user object for a given username
+
+        Searches the database for a user object with the given username ("jmorton")
+        This search is "unsafe" and will therefore throw errors if the user does not exist
+
+        Arguments:
+            username -- the short name of the school to be found
+
+        Errors:
+            ItemDoesNotExistError -- rasied if there is no item found
+            MultipleResultsFound -- rasied if there are multiple items of the same name (which should be unique) in the database, indicative of major issues
+
+        Return:
+            A user object as defined in the UserDatabase class
         """
-        pass
+        
+        with self.session_scope() as session:
+            try:
+                return session.query(self.user).get(userid)
+            except sqlalchemy.orm.exc.ObjectDeletedError:
+                raise ItemDoesNotExistError(DatabaseObjects.User, username)
+
 
     def fetch_course_by_name(self, coursename, school, semester=None, year=None, professor=None):
         """
+        returns a user object for a given username
+
+        Searches the database for a course object for a given school and user
+        This search is "unsafe" and will therefore throw errors if the course or school does not exist
+
+        Arguments:
+            coursename -- the short name of the course ("CS1301")
+            school -- the schoolname for the school where the course is held ("Georgia Tech")
+            semester -- (optional) the semester where the course is, a semester enum (Semesters.Summer)
+            year -- (optional) the year of the course (2012)
+            professor -- (optional) the professor who taught the course ("Greco")
+
+        Errors:
+            ItemDoesNotExistError -- rasied if there is no item found
+            MultipleResultsFound -- rasied if there are multiple items of the same name (which should be unique) in the database, indicative of major issues
+
+        Return:
+            A course object as defined in the CourseDatabase class
         """
-        pass
+
+        with self.session_scope() as session:
+            try:
+                schoolid = session.query(self.school).filter(self.school.school_short == school_name).one().school_id
+            except sqlalchemy.orm.exc.NoResultFound:
+                raise ItemDoesNotExistError(DatabaseObjects.School, school)
+
+            kwargs = {}
+            kwargs["course_name"] = coursename
+            kwargs["school_id"] = schoolid
+            kwargs["semeser"] = semeser
+            kwargs["year"] = year
+            kwargs["professor"] = professor
+
+            try:
+                return session.query(self.course).filter(**kwargs).one()
+            except:
+                raise ItemDoesNotExistError(DatabaseObjects.Course, coursename)
+
+
 
     def fetch_course_by_id(self, courseid):
         """
+        returns a user object for a given username
+
+        Searches the database for a course object for a given school and user
+        This search is "unsafe" and will therefore throw errors if the course does not exist
+
+        Arguments:
+            courseid -- the unique id for the course (21345)
+
+        Errors:
+            ItemDoesNotExistError -- rasied if there is no item found
+            MultipleResultsFound -- rasied if there are multiple items of the same name (which should be unique) in the database, indicative of major issues
+
+        Return:
+            A course object as defined in the CourseDatabase class
         """
-        pass
+        with self.session_scope() as session:
+            try:
+                session.query(self.course).get(courseid)
+            except sqlalchemy.orm.exc.ObjectDeletedError:
+                raise ItemDoesNotExistError(DatabaseObjects.Course, courseid)
+
 
     def fetch_rating_by_names(self, username, coursename, semester=None, year=None, professor=None):
         """
+        returns a rating object for a given user/course combination
+
+        Searches the database for a rating by a user for a specific course at their school
+        This search is unsafe and will therefore throw errors if the course, user, school, or rating does not exist
+
+        Arguments:
+            username -- the username of the user who rated the course ("jmorton")
+            coursename -- the short name of the course ("CS1301")
+            semester -- (optional) the semester where the course is, a semester enum (Semesters.Summer)
+            year -- (optional) the year of the course (2012)
+            professor -- (optional) the professor who taught the course ("Greco")
+
+        Errors:
+            ItemDoesNotExistError -- rasied if there is no item found
+            MultipleResultsFound -- rasied if there are multiple items of the same name (which should be unique) in the database, indicative of major issues
+
+        Return:
+            A rating object as defined in the RatingDatabase class
         """
-        pass
+
+
+        with self.session_scope() as session:
+            try:
+                user = session.query(self.user).filter(self.user.user_name == username).one()
+                userid = user.user_id
+                schoolid = school_school.id
+            except sqlalchemy.orm.exc.NoResultFound:
+                raise ItemDoesNotExistError(DatabaseObjects.User, username)
+
+            kwargs = {}
+            kwargs["course_name"] = coursename
+            kwargs["school_id"] = schoolid
+            kwargs["semeser"] = semeser
+            kwargs["year"] = year
+            kwargs["professor"] = professor
+
+            try:
+                courseid = session.query(self.course).filter(**kwargs).one().course_id
+            except:
+                raise ItemDoesNotExistError(DatabaseObjects.Course, coursename)
+
+            try:
+                return session.query(self.rating).filter(self.rating.user_id == userid).filter(self.rating.course_id == courseid).one()
+            except:
+                raise ItemDoesNotExistError(DatabaseObjects.Rating, username+" for "+coursename)
+
 
     def fetch_rating_by_id(self, userid, courseid):
         """
+        returns a rating object for a given userid/courseid combination
+
+        Searches the database for a rating by a user for a specific course at their school
+        This search is unsafe and will therefore throw errors if the course, user, school, or rating does not exist
+
+        Arguments:
+            userid -- the username of the user who rated the course (17)
+            courseid -- the short name of the course (12345)
+
+        Errors:
+            ItemDoesNotExistError -- rasied if there is no item found
+            MultipleResultsFound -- rasied if there are multiple items of the same name (which should be unique) in the database, indicative of major issues
+
+        Return:
+            A rating object as defined in the RatingDatabase class
         """
-        pass
+        with self.session_scope() as session:
+            try:
+                return session.query(self.rating).filter(self.rating.user_id == userid).filter(self.rating.course_id == courseid).one()
+            except:
+                raise ItemDoesNotExistError(DatabaseObjects.Rating, username+" for "+coursename)
 
     def school_exists(self, school):
         """
+
         """
         pass
 
@@ -197,6 +394,16 @@ class Database(object):
         pass
 
     def fetch_courses():
+        """
+        """
+        pass
+
+    def check_password(self, username, password):
+        """
+        """
+        return True
+
+    def update_password(self, username, new_password):
         """
         """
         pass
