d900fd2109cf18750c707fef6789a6f1d9d4ea13
Joshua Morton <joshua.morton13@gmail.com>
2014-07-24 00:37:22 +0200

    added an api endpoint to toggle admin and moderator rights, its secured

--- routing.py
+++ routing.py
@@ -22,6 +22,8 @@ from api.handlers.ApiSchool import ApiSchool
 from api.handlers.ApiSchools import ApiSchools
 from api.handlers.ApiUsers import ApiUsers
 from api.handlers.ApiUser import ApiUser
+from api.handlers.ApiToggleMod import ApiToggleMod
+from api.handlers.ApiToggleAdmin import ApiToggleAdmin
 
 
 from databases.database import Database
@@ -74,6 +76,9 @@ class_rank = Application([
     #     /api/schools/
     #     /api/schools.json
     (r'/api/users(:?/|\.json)?', ApiUsers, dict(db=db)),
+    (r'/api/toggle/admin/(\d+)/?', ApiToggleAdmin, dict(db=db)),
+    (r'/api/toggle/moderator/(\d+)/?', ApiToggleMod, dict(db=db)),
+
     (r'/api/?', ApiHome, dict(db=db)),
     # user/(#####)
     # school/(#####)
