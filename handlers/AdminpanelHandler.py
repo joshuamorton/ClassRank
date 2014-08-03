5e43329a67a2dca93879e3863736def2ff11d712
Joshua Morton <joshua.morton13@gmail.com>
2014-08-03 16:42:54 -0400

    updated the moderator interface to use websockets to add/remove moderators

--- handlers/AdminpanelHandler.py
+++ handlers/AdminpanelHandler.py
@@ -10,19 +10,22 @@ class AdminpanelHandler(BaseHandler):
     @authenticated
     def get(self):
         self.user = self.get_user_obj()
+        if self.user.admin:
+            self.data["auth"] = True
+            self.data["schools"] = self.db.schools
 
-        self.data["auth"] = True
-        self.data["schools"] = self.db.schools
-        #this I would consider the blackest of magicks
-        with self.db.session_scope() as session:
-            for school in self.data["schools"]:
-                session.add(school)
-                school.numcourses = len(school.courses)
-                school.numstudents = len(school.students)
-        self.data["users"] = self.db.users
-        self.data["user"] = self.user
+            #this I would consider the blackest of magicks
+            with self.db.session_scope() as session:
+                for school in self.data["schools"]:
+                    session.add(school)
+                    school.numcourses = len(school.courses)
+                    school.numstudents = len(school.students)
+            self.data["users"] = self.db.users
+            self.data["user"] = self.user
 
-        self.render("adminpanel.html", **self.data)
+            self.render("adminpanel.html", **self.data)
+        else:
+            self.redirect("/welcome")
 
 
     @authenticated
