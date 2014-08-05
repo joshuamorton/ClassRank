ce87296fd1a238e1b2a8beb2b813b26f105e95bf
Joshua Morton <joshua.morton13@gmail.com>
2014-08-05 00:42:57 -0400

    updated the dashboard to include ratings, now just to let people add them

--- handlers/DashHandler.py
+++ handlers/DashHandler.py
@@ -16,5 +16,6 @@ class DashHandler(BaseHandler):
             self.data["school_courses"] = self.db.fetch_school_by_id(session, self.user.school_id).courses
             session.add(self.user)
             self.data["your_courses"] = self.user.courses
+            self.data["your_ratings"] = {course.course_id : self.db.fetch_rating_by_id(self, session, self.data["user"].user_id, course.course_id) for course in self.data["your_courses"]}
         self.render("dash.html", **self.data)
 
